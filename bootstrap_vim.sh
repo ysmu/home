758ebaee4a88353c42d006209b4530057f536c28
yzmu <yzmuzz@gmail.com>
2024-05-10 11:37:23 -0700

    node

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -45,12 +45,11 @@ nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 
 # Install node
-echo "Installing node..."
-sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update && sudo apt-get install nodejs -y 2>/dev/null
+if ! command -v npm &> /dev/null; then
+  echo "Installing node..."
+  curl -fsSL https://deb.nodesource.com/setup_22.x | bash - &&\
+  apt-get install -y nodejs
+fi
 
 
 # pyright
